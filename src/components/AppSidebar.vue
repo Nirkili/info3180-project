10c04d4780bb5bc778fe239983dce21a78e2b032
Dana Archer <danamarcher@gmail.com>
2026-04-11 22:54:15 -0500

    Designed base template

---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,124 @@
+<script setup>
+  
+</script>
+
+<template>
+  <aside class="nav-bar">
+
+    <div class="user">
+       <img src="/images/profile.jpg" alt="pic">
+      <div class="name">Hi Alric!</div>
+     
+    </div>
+   
+
+    <div class="links">
+      <hr>
+      <ul>
+        <li>
+          <RouterLink to="" class=""><i class="fa-solid fa-house"></i>Home</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="" class=""><i class="fa-solid fa-heart"></i>Matches</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="" class=""><i class="fa-solid fa-bookmark"></i>Bookmarks</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="" class=""><i class="fa-solid fa-comment"></i>Messages</RouterLink>
+        </li>
+      </ul>
+
+    </div>
+  </aside>
+  
+  </template>
+
+  <style scoped>
+  
+
+  .nav-bar{
+    display: flex;
+    flex-direction:column;
+    width: 220px;
+    height: 100%;
+    background-color: rgb(255, 255, 255);
+    gap:20px;
+    border-radius: 20px;
+    overflow-y: auto;
+    box-shadow: 0 0 10px rgba(95, 10, 164, 0.1);
+  }
+
+  img{
+    border-radius: 100px;
+    height: 100px;
+  }
+
+  .name{
+    font-size: 1.5rem;
+    color:  rgb(85, 13, 103);
+    font-weight: 600px;
+  }
+
+  .user{
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-top: 40px;
+    gap: 20px;
+    margin-bottom: 20px;
+    }
+
+    .links i{
+      margin-right: 8px;
+      color:  rgb(85, 13, 103);
+    }
+
+    .links ul {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+    }
+
+    .links li{
+      margin-bottom: 35px;
+      text-align: center;
+      padding: 12px;
+      border-radius: 12px;
+      position: relative;
+    }
+
+    .links li:hover{
+      background-color:  rgb(238, 219, 243); 
+      padding: 15px;
+      cursor: pointer;
+    }
+
+    .links li:hover::before {
+      content: "";
+      position: absolute;
+      left: 0;
+      top: 8px;
+      bottom: 8px;
+      width: 4px;
+      background-color:  rgb(85, 13, 103); 
+      border-radius: 4px;
+      transition: all 0.25s ease;
+    }
+
+    .links a{
+      text-decoration: none;
+      border-radius: 20px;
+      color: black;
+
+
+    }
+
+    hr{
+      color:  rgb(85, 13, 103);
+    }
+
+  
+
+</style>
